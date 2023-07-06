c79b0bcedbab83005e43adf4da73aaff407c013b
bmwelu12 <98038088+bmwelu12@users.noreply.github.com>
2023-07-06 05:14:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def get_api_response(prompt: str) -> str | None:
         response: dict = openai.Completion.create(
             model='text-davinci-003',
             prompt=prompt,
-            temperature=0.9,
-            max_tokens=150,
+            temperature=0.8,
+            max_tokens=120,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0.6,
